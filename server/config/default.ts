94af28a9933b807b13840e0844e586115cb389c9
Lavisha S <work.lavishaa@gmail.com>
2025-09-13 11:36:47 +0000

    fe connected to be

--- server/config/default.ts
+++ server/config/default.ts
@@ -1,5 +1,10 @@
 export default {
 	port: 4000,
 	host: "0.0.0.0",
-	corsOrigin: ["http://localhost:3000", "https://chat-app-15el.vercel.app"],
+	corsOrigin: [
+		"http://localhost:3000",
+		"https://chat-app-15el.vercel.app",
+		"https://chat-app-git-main-lavisha-s-projects.vercel.app",
+		"https://chat-7qipdbo90-lavisha-s-projects.vercel.app"
+	],
 };
